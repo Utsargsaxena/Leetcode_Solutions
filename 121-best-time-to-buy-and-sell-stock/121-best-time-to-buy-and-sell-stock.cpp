8915db681a848a6225f3f73842d03a4033c12ddb
Utsarg Saxena <45102149+Utsargsaxena@users.noreply.github.com>
2022-02-01 23:41:21 +0530

    Time: 327 ms (5.14%), Space: 93.3 MB (90.32%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& a) {
-        int currdif=0;
-int maxdiff=0;
-int lowest=a[0];
-for(int i=1;i<a.size();i++){
-    if(a[i-1]>a[i])
-    {
-    currdif=0; 
-    lowest=min(lowest,a[i]);
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int minelem=prices[0];
+        for(int i=1;i<prices.size();i++){
+           minelem=min(prices[i],minelem);
+            profit=max(profit,(prices[i]-minelem));
+        }
+        return profit;
     }
-    
-    else
-    {    
-    if(a[i]>lowest)
-    currdif=a[i]-lowest;
-    else
-    currdif=a[i]-a[i-1];
-        
-    maxdiff=max(currdif,maxdiff);
-    }
-}
-        return maxdiff;
-    } 
 };
