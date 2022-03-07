cd75a0c7c0e26109d316d2c70cf95a64d6ad6cef
Utsarg Saxena <45102149+Utsargsaxena@users.noreply.github.com>
2022-03-07 20:11:15 +0530

    Time: 25 ms (16.67%), Space: 12.6 MB (8.33%) - LeetHub

---
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+        unordered_map<int, int> mp;
+
+    for(int i=0; i<nums.size()-1; i++){
+        if(nums[i]==key)
+            mp[nums[i+1]]++;    
+    }
+    
+    int mx = INT_MIN;
+    int target = -1;
+    for(auto &it: mp){
+        if(it.second > mx){         
+            mx = it.second;
+            target = it.first;     
+        }
+    }
+    
+    return target;
+}
+};
