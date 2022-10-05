a170fc099e7d1c2ce82441d46421e5cca8d913a4
Utsarg Saxena <45102149+Utsargsaxena@users.noreply.github.com>
2022-10-06 00:03:42 +0530

    Time: 11 ms (55.47%), Space: 10.4 MB (36.94%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<int> m(nums1.begin(), nums1.end());
         vector<int> res;
         for (auto a : nums2)
-            if (m.count(a)>0) {
+            if (m.count(a)) {
                 res.push_back(a);
                 m.erase(a);
             }
